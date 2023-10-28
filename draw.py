89f31e8844bb42a7b2199185ae3a44a349029768
Adam Tawfik <Adamttawfik@gmail.com>
2023-10-27 23:02:00 -0400

    Drawing menu change
    When you draw now it will automatically close the drawing menu.

--- draw.py
+++ draw.py
@@ -3,10 +3,13 @@ import os
 import pygame
 from PIL import Image
 
+# Initialize Pygame
+pygame.init()
+font = pygame.font.SysFont('Arial', 15)
+text = font.render('Draw a single digit number and I will recognize it:', True, (255, 255, 255))
+
 
 def main():
-    # Initialize Pygame
-    pygame.init()
 
     # Set the window size
     WIDTH, HEIGHT = 280, 280
@@ -24,6 +27,7 @@ def main():
     # Run the game loop
     running = True
     while running:
+        screen.blit(text, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 os.remove("Playerdrawing.jpg")
@@ -43,13 +47,25 @@ def main():
                 last_pos = event.pos
             elif event.type == pygame.MOUSEBUTTONUP:
                 # Stop drawing
+                if drawing:
+                    os.remove("Playerdrawing.jpg")
+                    pygame.image.save(screen, "Playerdrawing.jpg")
+                    with Image.open("Playerdrawing.jpg") as im:
+                        # Resize the image
+                        im_resized = im.resize((28, 28))
+                        # Save the resized image
+                        im_resized.save('Playerdrawing.jpg')
+                    im = Image.open('Playerdrawing.jpg')
+                    im_gray = im.convert('L')
+                    im_gray.save('Playerdrawinggray.jpg')
+                    running = False
                 drawing = False
             elif event.type == pygame.MOUSEMOTION:
                 # If we are drawing, draw a line from the previous mouse position to the current mouse position
                 if drawing:
                     pygame.draw.line(screen, (255, 255, 255), last_pos, event.pos, 28)
                     last_pos = event.pos
-                    pygame.display.update()
+        pygame.display.update()
 
     # Quit Pygame
     pygame.quit()

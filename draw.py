17f06a7304f83de287782ae8127db4c805cd9091
Adam Tawfik <Adamttawfik@gmail.com>
2023-11-28 17:58:09 -0500

    Added Saving

--- draw.py
+++ draw.py
@@ -1,13 +1,10 @@
 import os
-
 import pygame
 from PIL import Image
 
 # Initialize Pygame
 pygame.init()
 font = pygame.font.SysFont('Arial', 15)
-text = font.render('Draw a single digit number and I will recognize it:', True, (255, 255, 255))
-
 
 def main():
 
@@ -23,11 +20,11 @@ def main():
     # Initialize some variables
     drawing = False  # Keep track of whether we are drawing or not
     last_pos = (0, 0)  # Keep track of the previous mouse position
+    drawn_area_rect = None  # Bounding box of the drawn area
 
     # Run the game loop
     running = True
     while running:
-        screen.blit(text, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 os.remove("Playerdrawing.jpg")
@@ -63,9 +60,67 @@ def main():
             elif event.type == pygame.MOUSEMOTION:
                 # If we are drawing, draw a line from the previous mouse position to the current mouse position
                 if drawing:
-                    pygame.draw.line(screen, (255, 255, 255), last_pos, event.pos, 28)
+                    pygame.draw.line(screen, (255, 255, 255), last_pos, event.pos, 20)  # Adjusted line thickness
                     last_pos = event.pos
+
         pygame.display.update()
+
+    # Find the bounding box of the drawn area
+    drawn_area_rect = get_drawn_area_rect(screen)
+
+    # Copy the drawn area to a new surface
+    drawn_surface = pygame.Surface((drawn_area_rect.width, drawn_area_rect.height), pygame.SRCALPHA)
+    drawn_surface.blit(screen, (0, 0), area=drawn_area_rect)
+
+    # Create a new black window
+    new_screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    new_screen.fill((0, 0, 0))  # Fill with black color
+
+    # Calculate the position to paste the drawn content at the center
+    center_x = (WIDTH - drawn_area_rect.width) // 2
+    center_y = (HEIGHT - drawn_area_rect.height) // 2
+
+    # Paste the drawn content onto the new screen at the center position
+    new_screen.blit(drawn_surface, (center_x, center_y))
+
+    pygame.display.flip()
+
+    # Save the centered image
+    pygame.image.save(new_screen, "CenteredDrawing.jpg")
+    im = Image.open('CenteredDrawing.jpg')
+    im_gray = im.convert('L')
+    im_resized = im_gray.resize((28, 28))
+    im_resized.save('CenteredDrawinggray.jpg')
+
+    # Wait for a key press before quitting
+    wait_for_key()
 
     # Quit Pygame
     pygame.quit()
+
+def get_drawn_area_rect(screen):
+    # Get all non-black pixels in the screen
+    non_black_pixels = [(x, y) for x in range(screen.get_width()) for y in range(screen.get_height()) if screen.get_at((x, y)) != (0, 0, 0, 255)]
+
+    if non_black_pixels:
+        # Calculate the bounding box of the drawn area
+        min_x = min(x for x, y in non_black_pixels)
+        max_x = max(x for x, y in non_black_pixels)
+        min_y = min(y for x, y in non_black_pixels)
+        max_y = max(y for x, y in non_black_pixels)
+
+        return pygame.Rect(min_x, min_y, max_x - min_x, max_y - min_y)
+    else:
+        # If there are no non-black pixels, return an empty rectangle
+        return pygame.Rect(0, 0, 0, 0)
+
+def wait_for_key():
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                waiting = False
+            elif event.type == pygame.KEYDOWN:
+                waiting = False
+
--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from data import get_mnist
 import numpy as np
 import matplotlib.pyplot as plt
 import draw
-
+import keyboard
+import pandas as pd
+import os
 # Initialize weights and biases
+
 images, labels = get_mnist()
-w_i_h = np.random.uniform(-0.5, 0.5, (20, 784))
-w_h_o = np.random.uniform(-0.5, 0.5, (10, 20))
-b_i_h = np.zeros((20, 1))
+w_i_h = np.random.uniform(-0.5, 0.5, (1000, 784))
+w_h_o = np.random.uniform(-0.5, 0.5, (10, 1000))
+b_i_h = np.zeros((1000, 1))
 b_h_o = np.zeros((10, 1))
 leave = 0
-learn_rate = 0.04
+learn_rate = 0.01
 nr_correct_0 = 0
 nr_correct_1 = 0
 nr_correct_2 = 0
@@ -24,8 +27,17 @@ nr_correct_9 = 0
 nr_correct_all = 0
 nr_correct = [nr_correct_0, nr_correct_1, nr_correct_2, nr_correct_3, nr_correct_4, nr_correct_5, nr_correct_6,
               nr_correct_7, nr_correct_8, nr_correct_9, nr_correct_all]
-
+training = input("Do you want to train the network? (y/n)\n")
+run = True
 while True:
+    if training in ["y", "Y", "yes", "Yes", "YES"]:
+        break
+    elif training in ["n", "N", "no", "No", "NO"]:
+        run = False
+        break
+    else:
+        print("Please enter a valid input such as (y/n)")
+while run:
     for img, l in zip(images, labels):
         img.shape += (1,)
         l.shape += (1,)
@@ -49,28 +61,83 @@ while True:
         delta_h = np.transpose(w_h_o) @ delta_o * (h * (1 - h))
         w_i_h += -learn_rate * delta_h @ np.transpose(img)
         b_i_h += -learn_rate * delta_h
-
+        if keyboard.is_pressed('`'):
+            break
     # Show accuracy for this epoch
 
     print(f"Acc: {round((nr_correct[10] / images.shape[0]) * 100, 2)}%")
     for i in range(len(nr_correct) - 1):
-        if round((nr_correct[i] / images.shape[0]) * 100, 2) > 8.5:
+        if round((nr_correct[i] / images.shape[0]) * 100, 2) >= 9.9:
             leave += 1
             print(str(i) + ": passes with a " + str(round((nr_correct[i] / images.shape[0]) * 100, 2)) + "% accuracy")
             nr_correct[i] = 0
         else:
             print(f"Acc {i}: {round((nr_correct[i] / images.shape[0]) * 100, 2)}%")
             nr_correct[i] = 0
-    if leave >= 10:
+    if leave >= 10 or keyboard.is_pressed('`'):
         break
     else:
         leave = 0
         nr_correct[10] = 0
 
+while True:
+    move_on = input("Do you want to save the weights? (y/n)\n")
+    # Check if the DataFrame is empty
+    if move_on in ["y", "Y", "yes", "Yes", "YES"]:
+        df = pd.DataFrame(w_i_h)
+
+        # Specify the full path for the CSV file
+        file_path = r'C:\Code\Python\NumberIdentification\w_i_h.csv'
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        # Save the DataFrame to a CSV file, creating the file if it doesn't exist
+        df.to_csv(file_path, index=False, header=False, mode='x')
+
+        df = pd.DataFrame(w_h_o)
+
+        # Specify the full path for the CSV file
+        file_path = r'C:\Code\Python\NumberIdentification\w_h_o.csv'
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        # Save the DataFrame to a CSV file, creating the file if it doesn't exist
+        df.to_csv(file_path, index=False, header=False, mode='x')
+
+        df = pd.DataFrame(b_i_h)
+
+        # Specify the full path for the CSV file
+        file_path = r'C:\Code\Python\NumberIdentification\b_i_h.csv'
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        # Save the DataFrame to a CSV file, creating the file if it doesn't exist
+        df.to_csv(file_path, index=False, header=False, mode='x')
+
+        df = pd.DataFrame(b_h_o)
+
+        # Specify the full path for the CSV file
+        file_path = r'C:\Code\Python\NumberIdentification\b_h_o.csv'
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        # Save the DataFrame to a CSV file, creating the file if it doesn't exist
+        df.to_csv(file_path, index=False, header=False, mode='x')
+        break
+    elif move_on in ["n", "N", "no", "No", "NO"]:
+        # Load data from the CSV file into a DataFrame
+        file_path = r'C:\Code\Python\NumberIdentification\w_h_o.csv'
+        w_h_o = pd.read_csv(file_path, header=None).values
+        file_path = r'C:\Code\Python\NumberIdentification\w_i_h.csv'
+        w_i_h = pd.read_csv(file_path, header=None).values
+        file_path = r'C:\Code\Python\NumberIdentification\b_h_o.csv'
+        b_h_o = pd.read_csv(file_path, header=None).values
+        file_path = r'C:\Code\Python\NumberIdentification\b_i_h.csv'
+        b_i_h = pd.read_csv(file_path, header=None).values
+        break
+    else:
+        print("Please enter a valid input")
+
 # Show results
 while True:
     draw.main()
-    img = plt.imread("Playerdrawinggray.jpg")
+    img = plt.imread("CenteredDrawinggray.jpg")
 
     img.shape += (1,)
     # Forward propagation input into hidden
@@ -79,7 +146,8 @@ while True:
     # Forward propagation hidden into output
     o_pre = b_h_o + w_h_o @ h
     o = 1 / (1 + np.exp(-o_pre))
-
-    # Backpropagation output into hidden (cost function derivative)
-    l = np.zeros((10, 1))
     print("Is it: " + str(o.argmax()))
+    for i in range(len(o)):
+        print(f"{i}: {round(o[i][0] * 100, 2)}%")
+    if keyboard.is_pressed('`'):
+        break
